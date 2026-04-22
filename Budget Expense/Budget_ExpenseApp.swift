41fe1c67ed11e34538a805385f9b577ee9421946
Christian Gunawan <cg404cgunawan@gmail.com>
2026-04-23 02:15:43 +0700

    Initial Commit

---
+++ Budget Expense/Budget_ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_ExpenseApp.swift
+//  Budget Expense
+//
+//  Created by Christian Gunawan on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Budget_ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
